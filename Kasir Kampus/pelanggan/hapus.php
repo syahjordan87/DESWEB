42525e7f31bcddbfa37c259e60b35b43ea257dea
syahjordan87 <syahjordan87>
2026-09-22 12:46:17 +0700

    Kasir Kampus

--- Kasir Kampus/pelanggan/hapus.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$index = filter_input(INPUT_GET, 'index', FILTER_VALIDATE_INT);
-if ($index !== false && $index !== null && isset($_SESSION['pelanggan'][$index])) {
-    array_splice($_SESSION['pelanggan'], $index, 1);
-}
-header('Location: list.php');
-exit;
